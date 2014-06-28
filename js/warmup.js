aacec95524eff57b602f0109eea1b028a91981be
stevepm <steven.magelowitz@gmail.com>
2014-06-27 18:24:38 -0600

    LED light functional

--- js/warmup.js
+++ js/warmup.js
@@ -3,52 +3,52 @@ window.Warmup = {
   initialize: function () {
 
     /*
-      Description: show the names of the people at the top level
-      Outcome: Izaiah, Eulalia
-    */
+     Description: show the names of the people at the top level
+     Outcome: Izaiah, Eulalia
+     */
     showNamesForSelector("#top-level-button", "#root > li > span");
 
     /*
-      Description: show the names of the people at the second level
-      Outcome: Joel, Aaliyah, Claude
-    */
-    showNamesForSelector("#second-level-button", ___);
+     Description: show the names of the people at the second level
+     Outcome: Joel, Aaliyah, Claude
+     */
+    showNamesForSelector("#second-level-button", "#root > li > ul > li > span");
     //                       You need to replace ^^^ with a valid CSS selector
 
     /*
-      Description: show the names of the people who have data-employment="full-time"
-      Outcome: Izaiah, Della, Lowell
-    */
-    showNamesForSelector("#full-time-button", ___);
+     Description: show the names of the people who have data-employment="full-time"
+     Outcome: Izaiah, Della, Lowell
+     */
+    showNamesForSelector("#full-time-button", "span[data-employment='full-time']");
 
     /*
-      Description: show the names of the people with the class name "talkative"
-      Outcome: Claude, Lurline
-    */
-    showNamesForSelector("#talkative-button", ___);
+     Description: show the names of the people with the class name "talkative"
+     Outcome: Claude, Lurline
+     */
+    showNamesForSelector("#talkative-button", ".talkative");
 
     /*
-      Description: show the names of the people who have an id of "vp"
-      Outcome: Eulalia
-    */
-    showNamesForSelector("#vp-button", ___);
+     Description: show the names of the people who have an id of "vp"
+     Outcome: Eulalia
+     */
+    showNamesForSelector("#vp-button", "#vp");
 
     /*
-      Description: show the names of the people under Isaiah
-      Outcome: Della, Lurline, Aaliyah, Elisa
-    */
-    showNamesForSelector("#under-isaiah-button", ___);
+     Description: show the names of the people under Isaiah
+     Outcome: Della, Lurline, Aaliyah, Elisa
+     */
+    showNamesForSelector("#under-isaiah-button", "#root > li > ul:nth-child(1) > li > span");
 
     /*
-      Description: show the names of the people under Eulalia
-      Outcome: Claude, Lowell
-    */
-    showNamesForSelector("#under-eulalia-button", ___);
+     Description: show the names of the people under Eulalia
+     Outcome: Claude, Lowell
+     */
+    showNamesForSelector("#under-eulalia-button", "#root > li:nth-child(2) > span:nth-child(1n+2)");
 
     /*
-      Description: values of all of the input fields of type text
-      Outcome: Joe, Example
-    */
+     Description: values of all of the input fields of type text
+     Outcome: Joe, Example
+     */
     showValuesForSelector("#text-field-button", ___);
 
   }
